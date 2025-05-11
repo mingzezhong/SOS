27908aa95ce137eed191e9b703f1da15a8835094
mingzezhong <Mingze.Zhong@Student.uts.edu.au>
2025-05-12 09:01:38 +1000

    updata args

--- inference/main.py
+++ inference/main.py
@@ -29,8 +29,8 @@ parser = argparse.ArgumentParser(description="处理命令行参数")
 # 添加命令行参数
 parser.add_argument('--hf_model_name', type=str, default='deepseek-ai/DeepSeek-V2-Lite-Chat', help="hf_model_name")
 parser.add_argument('--sample_type', type=str, default='base', help="base, stratified, uniform")
-parser.add_argument('--num_movies', type=int, default=3, help="num_movies")
-parser.add_argument('--agents_per_movie', type=int, default=10, help="agents_per_movie")
+parser.add_argument('--num_movies', type=int, default=100, help="num_movies")
+parser.add_argument('--agents_per_movie', type=int, default=100, help="agents_per_movie")
 parser.add_argument('--rate_num', type=int, default=3, help="rate_num")
 parser.add_argument('--min_count', type=int, default=50, help="min_count")
 
